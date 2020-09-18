fdc59791ac06d908013a5d58c612f72823475f05
xiaohouzi0717 <1977305347@qq.com>
2020-09-18 10:01:06 +0800

    修改了manage

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@
 import os
 import sys
 
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
